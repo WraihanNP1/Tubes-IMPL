ed8db0a674a330eed86d7d797f94b3280c970f25
WraihanNP1 <windiraihan5@gmail.com>
2023-11-29 18:39:10 +0700

    update register, melakukan enkripsi password dan menghubungkan database

--- register.php
+++ register.php
@@ -1,15 +1,31 @@
+<?php
+
+require 'conection.php';
+
+if (isset($_POST["daftar"])) {
+
+    if (daftar($_POST) > 0 ) {
+        echo "<script>
+                    alert('user baru berhasil ditambahkan!');
+                </script>";
+    } else {
+       echo mysqli_error($conn);
+    } 
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
 <head>
     <meta charset="utf-8">
-    <title>Form Login</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Form Register</title>
+    <link rel="stylesheet" href="login.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <form action="login_controller.php" method="post" class="input-group">
+    <form action="" method="post" class="input-group">
         <div class="title">
             <b class="front">Daftar </b><b class="back">Akun</b>
         </div>
@@ -25,7 +41,7 @@
             <input type="password" name="confirm_password" required>
             <span data-placeholder="Confirm Password"></span>
         </div>
-        <button type="submit">Daftar</button>
+        <button type="submit" name="daftar">Daftar</button>
 
         <p style="margin-top: 129px;">
             <small>Sudah mempunyai akun? login <a href="login.html">di sini</a></small>
